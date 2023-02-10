c66e55a48a7f59c6d9f77002a8173f634b1a29f9
Diogo Guimaraes <mail@diogoguimaraes.pt>
2023-02-10 17:03:40 +0000

    signup post problems

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require("../models/User.model");
+
+const bcrypt = require('bcryptjs')
+
+
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("home");
@@ -11,9 +16,27 @@ router.get("/signup", (req, res, next) => {
   res.render("signup");
 });
 
+/* POST signup page */
+router.post("/signup", async (req, res, next) => {
+
+  const body = { ...req.body }
+
+  const salt = bcrypt.genSaltSync(10);
+  const passwordHash = bcrypt.hashSync(body.password, salt);
+  console.log(passwordHash)
+
+  delete body.password
+  body.passwordHash = passwordHash
+
+  await User.create(body)
+  res.redirect('/login');
+});
+
+
+
 /* GET login page */
 router.get("/login", (req, res, next) => {
-    res.render("login");
-  });
+  res.render("login");
+});
 
 module.exports = router;
--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET cocktail page */
+/* GET cocktail gallery page */
 router.get("/", (req, res, next) => {
   res.render("cocktailRecipes");
 });
 
-/* GET cocktail page */
+/* GET cocktail detail page */
 router.get("/:recipeId", (req, res, next) => {
   res.render("recipeDetails");
 });
