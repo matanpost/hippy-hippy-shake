4169ee5d2676d86bff95e785f417f2b7df51e3e8
Diogo Guimaraes <mail@diogoguimaraes.pt>
2023-02-10 16:13:30 +0000

    Schema update and Routes revised

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET landing page */
+/* GET cocktail page */
 router.get("/", (req, res, next) => {
   res.render("cocktailRecipes");
 });
+
+/* GET cocktail page */
+router.get("/:recipeId", (req, res, next) => {
+  res.render("recipeDetails");
+});
+
+
+
 
 module.exports = router;
