6206d0fc4b58d44f9cf2865cbd0770ae436bdab3
Matan Post <matanpost@gmail.com>
2023-02-12 19:11:06 +0100

    middle of post login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -7,10 +7,54 @@ router.get("/", (req, res, next) => {
 });
 
 /* GET signup page */
+router.get("/signup", (req, res, next) => {
+  res.render("signup");
+});
+
+/* POST signup page */
+router.post("/signup", async (req, res, next) => {
+
+  const body = { ...req.body }
+
+  const salt = bcrypt.genSaltSync(10);
+  const passwordHash = bcrypt.hashSync(body.password, salt);
+  console.log(passwordHash)
+
+  delete body.password
+  body.passwordHash = passwordHash
+
+  try{
+    await User.create(body)
+    res.redirect('/auth');
+  } catch (error){
+    console.log(error);
+  }
+});
 
 /* GET login page */
 router.get("/login", (req, res, next) => {
     res.render("login");
   });
+
+  /* POST login page */
+  router.post("/login", async (req, res, next) => {
+    try {
+      const userMatch = await User.find({username: req.boday.username})
+      if (userMatch.length){
+         // we have a user
+         const currentUser = userMatch[0]
+         if (bcrypt.compareSync(req.body.password, currentUser.passwordHash)){
+            //correct password
+            
+         } else {
+            // incorrect password
+         }
+      } else {
+         // we dont have a user
+      }
+    } catch (error){
+      console.log(error);
+    }
+  })
 
 module.exports = router;
