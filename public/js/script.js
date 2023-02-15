4979fe6e2aa6e95d84770dc38b479a8062210af1
Matan Post <matanpost@gmail.com>
2023-02-15 19:38:28 +0100

    landing page style not complete

--- public/js/script.js
+++ public/js/script.js
@@ -2,3 +2,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("hippy-hippy-shake JS imported successfully!");
 });
+
+function verifyAge() {
+  const birthdate = document.getElementById("birthdate").value;
+  const age = calculateAge(birthdate);
+  if (age >= 21) {
+    window.location.href = "/auth/";
+  } else {
+    alert("Sorry, you must be 21 or older to enter.");
+  }
+}
+
+function calculateAge(birthdate) {
+  const today = new Date();
+  const birthdateObj = new Date(birthdate);
+  let age = today.getFullYear() - birthdateObj.getFullYear();
+  const month = today.getMonth() - birthdateObj.getMonth();
+  if (month < 0 || (month === 0 && today.getDate() < birthdateObj.getDate())) {
+    age--;
+  }
+  return age;
+}
