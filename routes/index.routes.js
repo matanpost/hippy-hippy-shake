07faabbdfa9da724fb49e91950cb69f866451f16
Diogo Guimaraes <mail@diogoguimaraes.pt>
2023-02-17 08:39:30 +0000

    new pages - css update

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -6,6 +6,11 @@ router.get("/", (req, res, next) => {
   res.render("landing", {user: req.session.user || "undefined"});
 });
 
+/* GET landing page */
+router.get("/404", (req, res, next) => {
+  res.render("404", {user: req.session.user || "undefined"});
+});
+
 // router.get("/recipes/freerecipes", (req, res, next) => {
 //   res.render("cocktailRecipes", {user: req.session.user || "undefined"});
 // });
